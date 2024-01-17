4f7af320911e4a1955ff82d5174743ef21f3fb56
Aabis-Ahmed-Hassan <aabisahmedhassan3@gmail.com>
2024-01-17 22:16:28 +0500

    Done with DarkMode Screen

---
+++ lib/Others/dark_mode.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class DarkMode extends StatefulWidget {
+  const DarkMode({super.key});
+
+  @override
+  State<DarkMode> createState() => _DarkModeState();
+}
+
+class _DarkModeState extends State<DarkMode> {
+  var blackTheme = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.blue,
+        brightness: blackTheme ? Brightness.dark : Brightness.light,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+          centerTitle: true,
+          actions: [
+            Switch(
+              value: blackTheme,
+              onChanged: (value) {
+                blackTheme = value;
+                setState(() {});
+              },
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text('Weight in Kgs'),
+                  ),
+                ),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text('Height in Feet'),
+                  ),
+                ),
+                const TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text('Height in Inches'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 50,
+                  width: 250,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(
+            Icons.add,
+          ),
+        ),
+      ),
+    );
+  }
+}
